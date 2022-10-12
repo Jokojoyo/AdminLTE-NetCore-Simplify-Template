ea52e93101e4e6c49082cac6dc2263ba56de54bd
Thomas Junanta Ginting <thomas.ginting@id.wilmar-intl.com>
2022-10-12 20:51:47 +0700

    Inital Commit
    Inital Commit

---
+++ AdmintLte-Simplify-Template/AdmintLte-Simplify-Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdmintLte_Simplify_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
